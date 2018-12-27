1e7430002f7f14ea523cdf537483586d5cbd69d6
Francis Brosseau <fbrosseau@GENETEC.COM>
2018-12-26 22:25:56 -0500

    dummy test

---
+++ SimpleProcessFramework.Tests/Dummy.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SimpleProcessFramework.Tests
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+        }
+    }
+}
