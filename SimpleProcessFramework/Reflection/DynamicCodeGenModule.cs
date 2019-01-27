fe9320eed631ccb0c19cb33a6e9977c8af113a3f
Francis Brosseau <francis@malagauche.com>
2019-01-27 14:28:41 -0500

    Strong name everything

--- SimpleProcessFramework/Reflection/DynamicCodeGenModule.cs
+++ SimpleProcessFramework/Reflection/DynamicCodeGenModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
 using System.Reflection.Emit;
 
 namespace Spfx.Reflection
@@ -6,11 +7,21 @@ namespace Spfx.Reflection
     internal static class DynamicCodeGenModule
     {
         public const string DynamicModuleName = "Spfx.DynamicAssembly";
+        public const string DynamicModuleAssemblyIdentity = DynamicModuleName + SimpleProcessFrameworkPublicKey.Value;
+
         public static ModuleBuilder DynamicModule { get; }
 
         static DynamicCodeGenModule()
         {
-            AssemblyBuilder asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(DynamicModuleName), AssemblyBuilderAccess.Run);
+            var ms = new MemoryStream();
+            using (var s = typeof(DynamicCodeGenModule).Assembly.GetManifestResourceStream("Spfx.Properties.spfx.snk"))
+            {
+                s.CopyTo(ms);
+            }
+
+            var asmName = new AssemblyName(DynamicModuleName);
+            asmName.KeyPair = new StrongNameKeyPair(ms.ToArray());
+            AssemblyBuilder asm = AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Run);
             DynamicModule = asm.DefineDynamicModule(DynamicModuleName);
         }
     }
