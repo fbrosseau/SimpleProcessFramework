10a3d07106696071ddfd089116bcac972ea139bf
Francis Brosseau <francis@malagauche.com>
2019-06-15 13:04:43 -0400

    Cleanup the testapps

---
+++ TestApp.Netcore/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Spfx;
+using Spfx.Interfaces;
+using Spfx.Runtime.Server;
+using System.Threading.Tasks;
+
+namespace TestApp234
+{
+
+    public interface IZigZag
+    {
+        Task<string> Test();
+    }
+
+    public class ZigZag : AbstractProcessEndpoint, IZigZag
+    {
+        public Task<string> Test()
+        {
+            return Task.FromResult("Allo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new ProcessCluster();
+            c.MasterProcess.ProcessBroker.CreateEndpoint(new ProcessCreationRequest
+            {
+                MustCreateNew = true,
+                ProcessInfo = new ProcessCreationInfo
+                {
+                    ProcessKind = ProcessKind.Netfx32,
+                    ProcessUniqueId = "LOL"
+                }
+            }, new EndpointCreationRequest
+            {
+                EndpointId = "LOL",
+                EndpointType = typeof(IZigZag),
+                ImplementationType = typeof(ZigZag)
+            }).Wait();
+
+            var iii = c.PrimaryProxy.CreateInterface<IZigZag>("/LOL/LOL");
+            var yoyo = iii.Test().Result;
+
+
+            Console.ReadLine();
+        }
+    }
+}
