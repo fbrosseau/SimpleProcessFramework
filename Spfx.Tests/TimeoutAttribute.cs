357cb3c2cd1106d8927707c0006831b025dc42e3
Francis Brosseau <francis@malagauche.com>
2019-06-16 10:56:39 -0400

    typo

---
+++ Spfx.Tests/TimeoutAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spfx.Tests
+{
+#if NETCOREAPP
+    /// <summary>
+    /// https://github.com/nunit/nunit/issues/3282
+    /// </summary>
+    public class TimeoutAttribute : Attribute
+    {
+        public TimeoutAttribute(int defaultTestTimeout)
+        {
+        }
+    }
+#endif
+}
