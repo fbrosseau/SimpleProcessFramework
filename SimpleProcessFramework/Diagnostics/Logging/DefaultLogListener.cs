17dd2873c2d9fa9532b4f9d6dbcff3973c0f3fe6
Francis Brosseau <francis@malagauche.com>
2019-07-01 12:39:46 -0400

    Move loggers to different folder

---
+++ SimpleProcessFramework/Diagnostics/Logging/DefaultLogListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Spfx.Utilities.Diagnostics
+{
+    internal class DefaultLogListener : ILogListener
+    {
+        public void Log(string name, LogTraceLevel level, string message, Exception ex = null)
+        {
+            var now = DateTime.Now;
+            Console.WriteLine($"{now:T}|{name} [{level}]: {message}");
+        }
+    }
+}
---
+++ SimpleProcessFramework/Diagnostics/Logging/DefaultLoggerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Spfx.Utilities.Diagnostics
+{
+    internal class DefaultLoggerFactory : ILoggerFactory
+    {
+        private readonly ILogListener m_listener;
+
+        public DefaultLoggerFactory(ILogListener listener)
+        {
+            m_listener = listener;
+        }
+
+        public ILogger GetLogger(Type loggedType, bool uniqueInstance = false)
+        {
+            return new DefaultLogger(m_listener, loggedType.FullName);
+        }
+    }
+}
