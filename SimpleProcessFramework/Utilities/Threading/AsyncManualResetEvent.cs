d184a50068c92fbc9a6e24fef1334ebd9e6ff318
Francis Brosseau <francis@malagauche.com>
2019-08-31 13:09:30 -0400

    Fix asyncmanualresetevent to not allocate on regular Wait

--- SimpleProcessFramework/Utilities/Threading/AsyncManualResetEvent.cs
+++ SimpleProcessFramework/Utilities/Threading/AsyncManualResetEvent.cs
@@ -5,73 +5,25 @@ using System.Threading.Tasks;
 
 namespace Spfx.Utilities.Threading
 {
-    internal class AsyncManualResetEvent : Disposable
+    internal class AsyncManualResetEvent : BaseAsyncEvent
     {
-        private readonly object m_lock = new object();
-
-        private EventWaiter m_firstWaiter;
         private EventWaiter m_noCancellationWaiters;
 
-        private Exception m_disposeException;
-
         public bool IsSet { get; private set; }
-
-        private class EventWaiter : TaskCompletionSource<bool>, IDisposable
-        {
-            public EventWaiter Next;
-            public EventWaiter Previous;
-            private IDisposable m_ctRegistration; // boxing to avoid tearing
-            private Timer m_timer;
-            private readonly AsyncManualResetEvent m_owner;
-
-            private static readonly TimerCallback s_rawTimeoutRequest = RawOnCancelRequested;
-            private static readonly Action<object> s_rawCancelRequest = RawOnCancelRequested;
-
-            public EventWaiter(AsyncManualResetEvent e)
-                : base(TaskCreationOptions.RunContinuationsAsynchronously)
-            {
-                m_owner = e;
-            }
-
-            internal void Activate(CancellationToken ct, TimeSpan timeout)
-            {
-                if (ct.CanBeCanceled)
-                {
-                    m_ctRegistration = ct.Register(s_rawCancelRequest, this, false);
-                }
-
-                if (timeout != Timeout.InfiniteTimeSpan)
-                {
-                    m_timer = new Timer(s_rawTimeoutRequest, this, timeout, Timeout.InfiniteTimeSpan);
-                }
-
-                if (Task.IsCompleted)
-                    Dispose();
-            }
-
-            private static void RawOnCancelRequested(object s)
-            {
-                ((EventWaiter)s).OnCancelRequested();
-            }
-
-            private void OnCancelRequested()
-            {
-                TrySetCanceled();
-                m_owner.Cancel(this);
-            }
-
-            public void Dispose()
-            {
-                TrySetCanceled();
-                Thread.MemoryBarrier();
-                m_ctRegistration?.Dispose();
-                m_timer?.Dispose();
-            }
-        }
 
         public AsyncManualResetEvent(bool initialState = false)
         {
             IsSet = initialState;
+        }
+
+        protected override void OnDispose()
+        {
+            lock (m_lock)
+            {
+                UnblockAllWaiters(false);
+            }
+
+            base.OnDispose();
         }
 
         public void Set()
@@ -94,38 +46,20 @@ namespace Spfx.Utilities.Threading
             }
         }
 
-        public async ValueTask WaitAsync()
+        public ValueTask WaitAsync()
         {
-            await WaitAsync(CancellationToken.None);
+            return WaitInternal(default, Timeout.InfiniteTimeSpan).AsVoidValueTask();
         }
-
         public ValueTask<bool> WaitAsync(CancellationToken ct)
         {
-            return WaitAsync(ct, Timeout.InfiniteTimeSpan);
+            return WaitInternal(ct, Timeout.InfiniteTimeSpan).AsValueTask();
         }
-
         public ValueTask<bool> WaitAsync(TimeSpan timeout)
         {
-            return WaitAsync(CancellationToken.None, timeout);
+            return WaitInternal(CancellationToken.None, timeout).AsValueTask();
         }
 
-        protected override void OnDispose()
-        {
-            lock (m_lock)
-            {
-                UnblockAllWaiters(false);
-            }
-
-            base.OnDispose();
-        }
-
-        public void Dispose(Exception ex)
-        {
-            m_disposeException = ex;
-            Dispose();
-        }
-
-        private ValueTask<bool> WaitAsync(CancellationToken ct, TimeSpan timeout, EventWaiter waiter = null)
+        private WaitSyncOrAsyncResult WaitInternal(CancellationToken ct, TimeSpan timeout, EventWaiter waiter = null)
         {
             bool cancellable = timeout != Timeout.InfiniteTimeSpan || ct.CanBeCanceled;
             bool cancelled = ct.IsCancellationRequested || timeout == TimeSpan.Zero;
@@ -135,15 +69,15 @@ namespace Spfx.Utilities.Threading
                 ThrowIfDisposed();
 
                 if (IsSet)
-                    return new ValueTask<bool>(true);
+                    return new WaitSyncOrAsyncResult(true);
 
                 if (cancelled)
-                    return new ValueTask<bool>(false);
+                    return new WaitSyncOrAsyncResult(false);
 
                 if (!cancellable)
                 {
                     if (m_noCancellationWaiters != null)
-                        return new ValueTask<bool>(m_noCancellationWaiters.Task);
+                        return new WaitSyncOrAsyncResult(m_noCancellationWaiters.Task);
 
                     if (waiter != null)
                         m_noCancellationWaiters = waiter;
@@ -162,44 +96,22 @@ namespace Spfx.Utilities.Threading
             if (waiter != null)
             {
                 waiter.Activate(ct, timeout);
-                return new ValueTask<bool>(waiter.Task);
+                return new WaitSyncOrAsyncResult(waiter.Task);
             }
 
-            return WaitAsync(ct, timeout, new EventWaiter(this));
-        }
-
-        private void Cancel(EventWaiter eventWaiter)
-        {
-            lock (m_lock)
-            {
-                if (eventWaiter.Next != null)
-                    eventWaiter.Next.Previous = eventWaiter.Previous;
-                if (eventWaiter.Previous != null)
-                    eventWaiter.Previous.Next = eventWaiter.Next;
-
-                if (ReferenceEquals(m_firstWaiter, eventWaiter))
-                    m_firstWaiter = eventWaiter.Previous;
-            }
+            return WaitInternal(ct, timeout, new EventWaiter(this));
         }
 
         private void UnblockAllWaiters(bool success)
         {
-            var ex = m_disposeException;
-
             Debug.Assert(Monitor.IsEntered(m_lock));
+
             while (m_firstWaiter != null)
             {
                 if (success)
-                {
                     m_firstWaiter.TrySetResult(true);
-                }
                 else
-                {
-                    if (ex is null)
-                        m_firstWaiter.TrySetCanceled();
-                    else
-                        m_firstWaiter.TrySetException(new ObjectDisposedException("This object is disposed", ex));
-                }
+                    m_firstWaiter.TrySetCanceled();
 
                 m_firstWaiter = m_firstWaiter.Previous;
             }
---
+++ SimpleProcessFramework/Utilities/Threading/BaseAsyncEvent.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spfx.Utilities.Threading
+{
+    internal abstract class BaseAsyncEvent : Disposable
+    {
+        protected EventWaiter m_firstWaiter;
+        protected readonly object m_lock = new object();
+
+        protected readonly struct WaitSyncOrAsyncResult
+        {
+            private readonly bool SyncResult;
+            private readonly Task<bool> AsyncTask;
+
+            public WaitSyncOrAsyncResult(bool syncResult)
+            {
+                AsyncTask = default;
+                SyncResult = syncResult;
+            }
+
+            public WaitSyncOrAsyncResult(Task<bool> asyncTask)
+            {
+                SyncResult = default;
+                AsyncTask = asyncTask;
+            }
+
+            public ValueTask AsVoidValueTask()
+            {
+                if (AsyncTask is null)
+                    return default;
+                return new ValueTask(AsyncTask);
+            }
+
+            public ValueTask<bool> AsValueTask()
+            {
+                if (AsyncTask is null)
+                    return new ValueTask<bool>(SyncResult);
+                return new ValueTask<bool>(AsyncTask);
+            }
+        }
+
+        protected class EventWaiter : TaskCompletionSource<bool>, IDisposable
+        {
+            public EventWaiter Next;
+            public EventWaiter Previous;
+            private CancellationTokenRegistration m_ctRegistration;
+            private Timer m_timer;
+            private readonly BaseAsyncEvent m_owner;
+
+            private static readonly TimerCallback s_rawTimeoutRequest = RawOnCancelRequested;
+            private static readonly Action<object> s_rawCancelRequest = RawOnCancelRequested;
+
+            public EventWaiter(BaseAsyncEvent e)
+                : base(TaskCreationOptions.RunContinuationsAsynchronously)
+            {
+                m_owner = e;
+            }
+
+            internal void Activate(CancellationToken ct, TimeSpan timeout)
+            {
+                m_ctRegistration = ct.Register(s_rawCancelRequest, this, useSynchronizationContext: false);
+
+                if (timeout != Timeout.InfiniteTimeSpan)
+                {
+                    m_timer = new Timer(s_rawTimeoutRequest, this, timeout, Timeout.InfiniteTimeSpan);
+                }
+
+                // this instance may already be completed (success or cancelled) at this point, but a race condition
+                // may have happened where we allocated for the CT or for the timer, make sure we dispose them.
+                if (Task.IsCompleted)
+                    Dispose();
+            }
+
+            private static void RawOnCancelRequested(object s)
+            {
+                ((EventWaiter)s).OnCancelRequested();
+            }
+
+            private void OnCancelRequested()
+            {
+                if (TrySetCanceled())
+                    m_owner.Cancel(this);
+            }
+
+            public void Dispose()
+            {
+                m_ctRegistration.Dispose();
+                m_timer?.Dispose();
+                TrySetCanceled();
+            }
+        }
+
+        protected override void OnDispose()
+        {
+            lock (m_lock)
+            {
+                while (m_firstWaiter != null)
+                {
+                    m_firstWaiter.TrySetCanceled();
+                    m_firstWaiter = m_firstWaiter.Previous;
+                }
+            }
+
+            base.OnDispose();
+        }
+
+        private void Cancel(EventWaiter eventWaiter)
+        {
+            lock (m_lock)
+            {
+                if (eventWaiter.Next != null)
+                    eventWaiter.Next.Previous = eventWaiter.Previous;
+                if (eventWaiter.Previous != null)
+                    eventWaiter.Previous.Next = eventWaiter.Next;
+
+                if (ReferenceEquals(m_firstWaiter, eventWaiter))
+                    m_firstWaiter = eventWaiter.Previous;
+            }
+        }
+    }
+}
