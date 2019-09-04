3c7dad0305da0e967b73f4e15144a47a656d91dd
Francis Brosseau <francis@malagauche.com>
2019-09-03 21:05:43 -0400

    asynclock is non-allocating on the happy path

--- SimpleProcessFramework/Utilities/Threading/AsyncLock.cs
+++ SimpleProcessFramework/Utilities/Threading/AsyncLock.cs
@@ -3,14 +3,111 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 
 namespace Spfx.Utilities.Threading
 {
     internal sealed class AsyncLock : Disposable
     {
-        private class LockSession : ValueTaskCompletionSource<IDisposable>, IDisposable, IThreadPoolWorkItem
+        private readonly FreeLockSession m_freeLockSession;
+        private readonly Queue<LockSession> m_waiters;
+        private object m_activeLockSession;
+
+        public bool IsLockTaken { get; private set; }
+
+        public AsyncLock()
         {
-            private AsyncLock m_asyncLock;
+            m_waiters = new Queue<LockSession>();
+            m_freeLockSession = new FreeLockSession(this);
+        }
+
+        public ValueTask<IDisposable> LockAsync()
+        {
+            return LockInternal(null);
+        }
+
+        private ValueTask<IDisposable> LockInternal(LockSession lockSession)
+        {
+            if (IsLockTaken && lockSession is null)
+                lockSession = new LockSession(this);
+
+            object rawLockSession = lockSession;
+
+            lock (m_waiters)
+            {
+                if (IsDisposed)
+                {
+                    lockSession.Cancel(canCompleteSynchronously: true);
+                }
+                else if (IsLockTaken)
+                {
+                    m_waiters.Enqueue(lockSession);
+                }
+                else
+                {
+                    IsLockTaken = true;
+
+                    if (lockSession is null)
+                        rawLockSession = m_freeLockSession;
+                    else
+                        lockSession.Unblock(canCompleteSynchronously: true);
+
+                    m_activeLockSession = rawLockSession;
+                }
+            }
+
+            if (rawLockSession is null)
+                return LockInternal(new LockSession(this));
+
+            if (rawLockSession is FreeLockSession freeLock)
+                return freeLock.CreateNextValueTask(this);
+
+            return new ValueTask<IDisposable>(lockSession.Task);
+        }
+
+        private void ExitLock(object completedSession)
+        {
+            LockSession completedWaiter = null;
+
+            lock (m_waiters)
+            {
+                if (m_waiters.Count > 0)
+                    completedWaiter = m_waiters.Dequeue();
+                else
+                    IsLockTaken = false;
+
+                m_activeLockSession = completedSession;
+            }
+
+            completedWaiter?.Unblock();
+        }
+
+        protected override void OnDispose()
+        {
+            List<LockSession> waiters = null;
+            lock (m_waiters)
+            {
+                if (m_waiters.Count > 0)
+                    waiters = m_waiters.ToList();
+            }
+
+            if (waiters != null)
+            {
+                ThreadPool.QueueUserWorkItem(s =>
+                {
+                    foreach (var waiter in (List<LockSession>)s)
+                    {
+                        waiter.Cancel(canCompleteSynchronously: true);
+                    }
+                }, waiters);
+            }
+
+            base.OnDispose();
+        }
+
+        private class LockSession : TaskCompletionSource<IDisposable>, IDisposable, IThreadPoolWorkItem
+        {
+            protected AsyncLock m_asyncLock;
             private volatile bool m_completeSynchronously;
 
             public LockSession(AsyncLock asyncLock)
@@ -53,83 +150,122 @@ namespace Spfx.Utilities.Threading
                 // it's very important that we don't ExitLock more than once
                 // from this instance! 
                 var lockInstance = m_asyncLock != null ? Interlocked.Exchange(ref m_asyncLock, null) : null;
-                lockInstance?.ExitLock();
+                lockInstance?.ExitLock(this);
             }
         }
 
-        private readonly Queue<LockSession> m_waiters;
-
-        public bool IsLockTaken { get; private set; }
-
-        public AsyncLock()
+        private class FreeLockSession : IValueTaskSource<IDisposable>, IDisposable, IThreadPoolWorkItem
         {
-            m_waiters = new Queue<LockSession>();
-        }
+            private AsyncLock m_asyncLock;
+            private short m_token;
 
-        public ValueTask<IDisposable> LockAsync()
-        {
-            var session = new LockSession(this);
+            private Action<object> m_continuationToInvoke;
+            private object m_continuationToInvokeState;
 
-            lock (m_waiters)
+            public FreeLockSession(AsyncLock asyncLock)
             {
-                if (IsDisposed)
-                {
-                    session.Cancel(canCompleteSynchronously: true);
-                }
-                else if (IsLockTaken)
-                {
-                    m_waiters.Enqueue(session);
-                }
-                else
-                {
-                    IsLockTaken = true;
-                    session.Unblock(canCompleteSynchronously: true);
-                }
+                m_asyncLock = asyncLock;
             }
 
-            return session.ValueTask;
-        }
-
-        private void ExitLock()
-        {
-            LockSession completedWaiter = null;
-
-            lock (m_waiters)
+            internal ValueTask<IDisposable> CreateNextValueTask(AsyncLock asyncLock)
             {
-                if (m_waiters.Count > 0)
-                {
-                    completedWaiter = m_waiters.Dequeue();
-                }
-                else
-                {
-                    IsLockTaken = false;
-                }
+                m_asyncLock = asyncLock;
+                m_continuationToInvoke = null;
+                return new ValueTask<IDisposable>(this, ++m_token);
             }
 
-            completedWaiter?.Unblock();
-        }
-
-        protected override void OnDispose()
-        {
-            List<LockSession> waiters = null;
-            lock (m_waiters)
+            void IDisposable.Dispose()
             {
-                if (m_waiters.Count > 0)
-                    waiters = m_waiters.ToList();
+                // it's very important that we don't ExitLock more than once
+                // from this instance! 
+                var lockInstance = m_asyncLock != null ? Interlocked.Exchange(ref m_asyncLock, null) : null;
+                lockInstance?.ExitLock(this);
             }
 
-            if (waiters != null)
+            IDisposable IValueTaskSource<IDisposable>.GetResult(short token)
             {
-                ThreadPool.QueueUserWorkItem(s =>
+                CheckToken(token);
+
+                m_continuationToInvoke = null;
+                m_continuationToInvokeState = null;
+
+                return this;
+            }
+
+            ValueTaskSourceStatus IValueTaskSource<IDisposable>.GetStatus(short token)
+            {
+                CheckToken(token);
+                return ValueTaskSourceStatus.Succeeded;
+            }
+
+            void IValueTaskSource<IDisposable>.OnCompleted(Action<object> continuation, object state, short token, ValueTaskSourceOnCompletedFlags flags)
+            {
+                CheckToken(token);
+
+                if (Interlocked.Exchange(ref m_continuationToInvoke, continuation) != null)
+                    ThrowInvalidOperationException();
+
+                m_continuationToInvoke = continuation;
+                m_continuationToInvokeState = state;
+
+                if ((flags & ValueTaskSourceOnCompletedFlags.UseSchedulingContext) != 0)
                 {
-                    foreach (var waiter in (List<LockSession>)s)
+                    var sc = SynchronizationContext.Current;
+                    if (sc != null && sc.GetType() != typeof(SynchronizationContext))
                     {
-                        waiter.Cancel(canCompleteSynchronously: true);
+                        sc.Post(s =>
+                        {
+                            ((FreeLockSession)s).InvokeCallback();
+                        }, this);
+                        return;
                     }
-                }, waiters);
+
+                    var ts = TaskScheduler.Current;
+                    if (ts != TaskScheduler.Default)
+                    {
+                        Task.Factory.StartNew(continuation, state, CancellationToken.None, TaskCreationOptions.DenyChildAttach, ts);
+                        return;
+                    }
+                }
+
+                if ((flags & ValueTaskSourceOnCompletedFlags.FlowExecutionContext) != 0)
+                {
+#if NETCOREAPP2_1_PLUS || NETSTANDANDARD2_1_PLUS
+                    ThreadPool.QueueUserWorkItem(continuation, state, preferLocal: true);
+#else
+                    ThreadPool.QueueUserWorkItem(s => ((FreeLockSession)s).InvokeCallback(), this);;
+#endif
+                }
+                else
+                {
+#if NETCOREAPP3_0_PLUS || NETSTANDARD2_1_PLUS
+                    ThreadPool.UnsafeQueueUserWorkItem(this, preferLocal: true);
+#else
+                    ThreadPool.UnsafeQueueUserWorkItem(s => ((FreeLockSession)s).InvokeCallback(), this);
+#endif
+                }
             }
 
-            base.OnDispose();
+            private void CheckToken(short token)
+            {
+                if (token != m_token)
+                    ThrowInvalidOperationException();
+            }
+
+            private void ThrowInvalidOperationException()
+            {
+                throw new InvalidOperationException();
+            }
+
+            private void InvokeCallback()
+            {
+                m_continuationToInvoke(m_continuationToInvokeState);
+            }
+
+            void IThreadPoolWorkItem.Execute()
+            {
+                InvokeCallback();
+            }
         }
     }
 }
--- SimpleProcessFramework/Utilities/Threading/ValueTaskCompletionSource.cs
+++ SimpleProcessFramework/Utilities/Threading/ValueTaskCompletionSource.cs
@@ -1,41 +1,59 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 
 namespace Spfx.Utilities.Threading
 {
 #if !TODO
+#if !NETCOREAPP3_0_PLUS && !NETSTANDARD2_1_PLUS
     internal class ValueTaskCompletionSource<TResult> : TaskCompletionSource<TResult>
     {
-        public ValueTask<TResult> ValueTask => new ValueTask<TResult>(Task);
+           public ValueTask<TResult> ValueTaskOfT => new ValueTask<TResult>(Task);
+        public ValueTask ValueTask => new ValueTask(Task);
 
-        public ValueTaskCompletionSource()
-        {
-        }
-
-        public ValueTaskCompletionSource(bool completeAsynchronously)
+        public ValueTaskCompletionSource(bool completeAsynchronously = false)
             : base(completeAsynchronously ? TaskCreationOptions.RunContinuationsAsynchronously : TaskCreationOptions.None)
         {
         }
     }
 #else
-    internal class ValueTaskCompletionSource<TResult> : IValueTaskSource<TResult>
+    internal class ValueTaskCompletionSource<TResult> : IValueTaskSource<TResult>, IValueTaskSource
     {
-        private static int s_nextToken;
-
         private ManualResetValueTaskSourceCore<TResult> m_source;
-        public ValueTask<TResult> ValueTask => new ValueTask<TResult>(this, (short)Interlocked.Increment(ref s_nextToken));
 
-        public ValueTaskCompletionSource()
-        {
-        }
+        public ValueTask<TResult> ValueTaskOfT => new ValueTask<TResult>(this, m_source.Version);
+        public ValueTask ValueTask => new ValueTask(this, m_source.Version);
 
-        public ValueTaskCompletionSource(bool completeAsynchronously)
+        public ValueTaskCompletionSource(bool completeAsynchronously = false)
         {
             m_source.RunContinuationsAsynchronously = completeAsynchronously;
         }
 
-        public TResult GetResult(short token) => m_source.GetResult(token);
+        protected void ResetValueTaskSource()
+        {
+            m_source.Reset();
+        }
+
+        public void TrySetCanceled()
+        {
+            TrySetException(new TaskCanceledException());
+        }
+
+        public void TrySetException(Exception ex)
+        {
+            m_source.SetException(ex);
+        }
+
+        public void TrySetResult(TResult result)
+        {
+            m_source.SetResult(result);
+        }
+
         public ValueTaskSourceStatus GetStatus(short token) => m_source.GetStatus(token);
         public void OnCompleted(Action<object> continuation, object state, short token, ValueTaskSourceOnCompletedFlags flags) => m_source.OnCompleted(continuation, state, token, flags);
+        void IValueTaskSource.GetResult(short token) => GetResult(token);
+        public TResult GetResult(short token) => m_source.GetResult(token);
     }
+#endif
 #endif
 }
